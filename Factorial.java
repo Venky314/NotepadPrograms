e0a87bf78171ca03a0a5dce2f674b0bc24e6f340
venkatesh <lankevenkatesh314@gmail.com>
2025-08-24 13:50:01 +0530

    Initial commit - added Notepad Programs

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Factorial
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Enter the any number:-");
+		
+		Scanner sc=new Scanner(System.in);
+		int a =sc.nextInt();
+		
+		System.out.print(a);
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
